f3fdd185fbcd9a72f756677159fe06031d3158df
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-05-25 00:11:43 +0545

    DS: Linked List

--- Data Structures/DS004.cpp
+++ Data Structures/DS004.cpp
@@ -67,6 +67,58 @@ void pushAt(Node** head, int pos, int data){
             head = &(*head) -> next;
         }
     }
+    ++listSize;
+}
+
+// Delete node by key
+void deleteNode(Node** head, int key) {
+    Node* temp = *head;
+    Node* prev = NULL;
+
+    if(temp != NULL && temp -> data == key) {
+        *head = temp -> next;
+        delete temp;
+        --listSize;
+        return;
+    } else {
+        while(temp != NULL && temp -> data != key){
+            prev = temp;
+            temp = temp -> next;
+        }
+
+        if(temp == NULL){
+            cout << "Key not found! \n";
+            return;
+        };
+
+        prev -> next = temp -> next;
+        delete temp;
+        --listSize;
+    }
+}
+
+// Delete node by position
+void deletePos(Node** head, int pos){
+    if(pos < 0 || *head == NULL) return;
+    
+    Node* temp = *head;
+
+    if(pos == 0){
+        *head = temp -> next;
+        delete temp;
+        --listSize;
+        return;
+    }
+
+    while(--pos && temp != NULL)
+        temp = temp -> next;
+
+    if (temp == NULL || temp -> next == NULL) return;
+
+    Node* target = temp -> next;
+    temp -> next = target -> next;
+    delete target;
+    --listSize;
 }
 
 int main(){
@@ -79,6 +131,9 @@ int main(){
     pushBack(&head, 5);
 
     pushAt(&head, 3, 3);
+
+    deletePos(&head, 4);
+    deleteNode(&head, 6);
 
     display(head);
 
